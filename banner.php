ba9093dbf9ecfc518e47af6b676e286d56a38f07
FajarFirdaus732 <35714197+FajarTheGGman@users.noreply.github.com>
2019-01-26 00:39:37 +0700

    Add files via upload

---
+++ banner.php
@@ -0,0 +1,11 @@
+<?php
+@system("apt-get install figlet");
+@system("figlet IslamPray");
+echo "[=======================]
+Coder : Fajar Firdaus
+FB : Fajar Firdaus
+IG : fajar_firdaus_7
+YT : iTech7732
+[=======================]
+";                         
+?>
